492714056e2c42a61e097d8ec55afc333075a1ac
OtaniToma <otani.toma@gmail.com>
2023-04-10 16:39:48 +0900

    10 sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
-      discord
+      <Sidebar />
     </div>
   );
 }
