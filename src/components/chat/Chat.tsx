f7a24ad2ba95e6afad680089ddcc8266af9e2baf
OtaniToma <otani.toma@gmail.com>
2023-04-11 18:25:27 +0900

    20. chat header

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './Chat.scss';
+import ChatHeader from './ChatHeader';
 
 function Chat() {
   return (
     <div className='chat'>
-      
+      <ChatHeader />
+      <div className='chatMessage'></div>
+      <div className='chatInput'></div>
     </div>
   )
 }
