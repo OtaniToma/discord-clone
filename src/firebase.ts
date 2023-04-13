aa8a51831d2ea4aa50857dd8eb6c30dbbb9b011e
OtaniToma <otani.toma@gmail.com>
2023-04-13 22:09:05 +0900

    27. setup firebase

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore/lite";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAxsXb7GZEdQUGcb2lpyf_VubQGcrZckhc",
+  authDomain: "discord-clone-9b3df.firebaseapp.com",
+  projectId: "discord-clone-9b3df",
+  storageBucket: "discord-clone-9b3df.appspot.com",
+  messagingSenderId: "247367103830",
+  appId: "1:247367103830:web:f0fbab19be43b55540d2f7"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db };
