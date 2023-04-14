a49402f8882614ac6ada0ff2baae8b74d342477d
OtaniToma <otani.toma@gmail.com>
2023-04-14 21:42:11 +0900

    35. dispatch user info and update state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import { useAppSelector } from './app/hooks';
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from './app/hooks';
 import './App.scss';
 import Login from './components/login/Login';
 import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/chat/Chat';
+import { auth } from './firebase';
+import { login, logout } from './features/userSlice';
 
 function App() {
 
   const user = useAppSelector((state => state.user));
   // const user = null;
-  console.log(user);
+  // console.log(user);
+
+  const dispatch = useAppDispatch();
+  
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      console.log(loginUser);
+      if (loginUser) {
+        dispatch(
+          login({
+          uid: loginUser.uid,
+          photo: loginUser.photoURL,
+          email: loginUser.email,
+          displayName: loginUser.displayName
+        })
+        );
+      } else {
+        dispatch(logout());
+      }
+    })
+  }, [dispatch]);
 
   return (
     <div className="App">
