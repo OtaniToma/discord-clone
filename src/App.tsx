532ff6da6e032189eeb998557143f3cdc19835a8
OtaniToma <otani.toma@gmail.com>
2023-04-14 17:01:47 +0900

    32. add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import './App.scss';
+import Login from './components/login/Login';
 import Sidebar from './components/sidebar/Sidebar';
 import Chat from './components/chat/Chat';
+import { useSelector } from 'react-redux';
 
 function App() {
+
+  // const user = useSelector((state => state.user.user));
+  const user = null;
+
   return (
     <div className="App">
-      <Sidebar />
-
-      <Chat />
+      {user ? (
+        <>
+          <Sidebar />
+          <Chat />
+        </>
+      ) : (
+        <>
+          <Login />
+        </>
+      )}
     </div>
   );
 }
