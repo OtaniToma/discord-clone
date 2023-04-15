16d2db8063d25c7098a743de341bf35bfaf07096
OtaniToma <otani.toma@gmail.com>
2023-04-15 18:40:19 +0900

    45. add new channel function

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -9,11 +9,22 @@ import HeadphonesIcon from '@mui/icons-material/Headphones';
 import SettingsIcon from '@mui/icons-material/Settings';
 import { useAppSelector } from '../../app/hooks';
 import useCollection from '../../hooks/useCollection';
+import { addDoc, collection } from 'firebase/firestore';
 // import { collection, query } from 'firebase/firestore/lite';
 
 const Sidebar = () => {
   const user = useAppSelector((state) => state.user);
   const { documents: channels } = useCollection('channels');
+
+  const addChannel = async () => {
+    let channelName: string | null = prompt('新しいチャンネルを作成します');
+
+    if(channelName) {
+      await addDoc(collection(db, 'channels'), {
+        channelName: channelName,
+      })
+    }
+  }
 
   return (
     <div className='sidebar'>
@@ -39,7 +50,7 @@ const Sidebar = () => {
               <ExpandMoreIcon />
               <h4>プログラミングチャンネル</h4>
             </div>
-            <AddIcon className='sidebarAddIcon' />
+            <AddIcon className='sidebarAddIcon' onClick={() => addChannel()} />
           </div>
 
           <div className='sidebarChannelList'>
