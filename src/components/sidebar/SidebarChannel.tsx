397ae9641ba3a5ce9e526fb095a27b6861be378e
OtaniToma <otani.toma@gmail.com>
2023-04-15 16:46:20 +0900

    43. show channel data 2

--- src/components/sidebar/SidebarChannel.tsx
+++ src/components/sidebar/SidebarChannel.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import './SidebarChannel.scss';
+import { DocumentData } from 'firebase/firestore';
 
-function SidebarChannel() {
+type Props = {
+  id: string,
+  channel: DocumentData
+}
+
+function SidebarChannel(props: Props) {
+  const { id, channel } = props;
+
   return (
     <div className='sidebarChannel'>
       <h4>
         <span className='sidebarChannelHash'>#</span>
-        Udemy
+        {channel.channel.channelName}
       </h4>
     </div>
   )
